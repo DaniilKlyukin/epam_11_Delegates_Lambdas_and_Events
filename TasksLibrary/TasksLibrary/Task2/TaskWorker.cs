ec0517f81cfd5f69137d56f28bada847d5e7ca11
Daniil <anatoliikljukin@mail.ru>
2019-06-30 12:03:17 +0400

    Create TaskWorker.cs
    Bubble sort call method.

---
+++ TasksLibrary/TasksLibrary/Task2/TaskWorker.cs
@@ -0,0 +1,8 @@
+﻿namespace TasksLibrary
+{
+    public class TaskWorker
+    {
+        public void BubbleSort(int[,] array, ISortable sortMethod, Direction d)
+            => sortMethod.Sort(array, d);
+    }
+}
