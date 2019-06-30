d15379715814981c3feca12a0508b7d555e2696d
Daniil <anatoliikljukin@mail.ru>
2019-06-30 12:02:20 +0400

    Create Evaluator.cs

---
+++ TasksLibrary/TasksLibrary/Task2/Evaluator.cs
@@ -0,0 +1,24 @@
+﻿namespace TasksLibrary
+{
+    public abstract class Evaluator
+    {
+        public void BubbleSort(int[] arr, Direction d)
+        {
+            for (int i = 1; i < arr.Length; i++)
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[j].CompareTo(arr[i]) == (int)d)
+                        Swap(arr, i, j);
+                }
+        }
+
+        protected void Swap<T>(T[] arr, int i, int j)
+        {
+            T temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        public abstract int[] Evaluate(int[,] arr, int row);
+    }
+}
