ebf92edfe32a25ba821bebcbb406ceef1df19930
Daniil <anatoliikljukin@mail.ru>
2019-07-06 21:44:34 +0400

    Create Person.cs

---
+++ TasksLibrary/Task3Test/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace Task3Test
+{
+    using System;
+    using TasksLibrary;
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public void PrintMessage(object sender, CounterEventArgs e)
+        {
+            Console.WriteLine($"{Name}: {e.Message}");
+        }
+    }
+}
