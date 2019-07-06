031ee9057d1288c2ac44f62d8a51f20a032f2e89
Daniil <anatoliikljukin@mail.ru>
2019-07-06 21:45:04 +0400

    Create Program.cs
    Console app for testing 3rd task.

---
+++ TasksLibrary/Task3Test/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task3Test
+{
+    using TasksLibrary;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = new Counter();
+
+            var person1 = new Person("Bob");
+            var person2 = new Person("Emma");
+
+            counter.onTimeOver += person1.PrintMessage;
+            counter.onTimeOver += person2.PrintMessage;
+
+            counter.CountDown(3, "Hello");
+        }
+    }
+}
