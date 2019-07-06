a0a7a461d2a0d2d7267345c04ed3a928cacad19b
Daniil <anatoliikljukin@mail.ru>
2019-07-06 21:42:31 +0400

    Create Counter.cs
    Create CountDown class

---
+++ TasksLibrary/TasksLibrary/Task3/Counter.cs
@@ -0,0 +1,47 @@
+﻿namespace TasksLibrary
+{
+    using System;
+    using System.Threading;
+
+    public class CounterEventArgs : EventArgs
+    {
+        public CounterEventArgs(string message, int time)
+        {
+            Message = message;
+            WaitTime = time;
+        }
+
+        public string Message { get; set; }
+
+        public int waitTime;
+        public int WaitTime
+        {
+            get
+            {
+                return waitTime;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException();
+
+                waitTime = value;
+            }
+        }
+    }
+
+    public class Counter
+    {
+        public event EventHandler<CounterEventArgs> onTimeOver;
+
+        public void CountDown(int seconds, string message)
+        {
+            if (seconds < 0)
+                throw new ArgumentException();
+
+            Thread.Sleep(1000 * seconds);
+
+            onTimeOver(this, new CounterEventArgs(message, seconds));
+        }
+    }
+}
